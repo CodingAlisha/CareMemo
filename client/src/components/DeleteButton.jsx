0442a7b31a8ac9c60152a4a101c96f8fe9268b39
CodingAlisha <alishasanguinetti@gmail.com>
2026-06-28 16:17:11 -0400

    updated delete btn response

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -5,12 +5,12 @@ const DeleteButton = ({ endpoint, id, onDelete }) => {
 
     const handleDelete = async () => {
       try {
-        await fetch(`/api/${endpoint}/${id}`, { method: "DELETE" });
+        const response = await fetch(`/api/${endpoint}/${id}`, { method: "DELETE" });
 
         if (!response.ok) {
           throw new Error(`Failed to delete: ${response.status}`);
         }
-        
+
         onDelete(id);
       } catch (error) {
         console.error("Error deleting item", error);
