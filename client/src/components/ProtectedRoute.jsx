efae1e32acb71d70f8815e244c91de4368cb69da
CodingAlisha <alishasanguinetti@gmail.com>
2026-06-29 17:39:36 -0400

    client user api update

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -6,7 +6,7 @@ const ProtectedRoute = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
   useEffect(() => {
-    api.get(`${import.meta.env.VITE_API_URL}/api/checkUser`, {
+    api.get('api/checkUser', {
       credentials: 'include'
     })
       .then(() => setIsAuthenticated(true))
